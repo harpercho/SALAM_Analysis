907e57583e0f5ec538e913f9a8ed0f8b7d5e08c3
Harper <hc2347@nyu.edu>
2021-02-08 10:16:08 +0400

    recreated stellar mass function plot.

--- main/visualization/make_plots.py
+++ main/visualization/make_plots.py
@@ -1,3 +1,12 @@
+import numpy as np
+import pynbody
+import pynbody.filt as filt
+import pynbody.units as units
+import pynbody.analysis.profile as profile
+import matplotlib.pyplot as plt
+import sys, os, glob, pickle, struct
+import plot_tools
+
 def plot_HMF(simpath, entry, ax=None):
     """Plot the halo mass function. If there are no axes given, make a figure and axes"""
     
@@ -22,7 +31,7 @@ def plot_HMF(simpath, entry, ax=None):
     
     plt.savefig("/scratch/hc2347/reports/60/HMF/z{}.png".format(z))
     
-def plot_SMF(entry):
+def plot_SMF(entry, bins):
     """Plot the stellar mass function and save reports directory."""
     
     z = entry["zred"]
@@ -31,7 +40,7 @@ def plot_SMF(entry):
     fig, ax = plt.subplots(figsize=(7,5), dpi = 150)
     
     # Plot Stellar Mass Function from density
-    x, smf = plot_tools.plot_mf(ax, stellar_mass_density, 50, entry["zred"])
+    x, smf = plot_tools.plot_mf(ax, stellar_mass_density, bins, entry["zred"])
     ax.plot(x, smf, label = 'SALAM z{}'.format(z))
     ax.set_ylabel('number density [Mpc $^{-3}$]')
     ax.set_xlabel('log$_{10}$(M) [M$_\odot$]')
@@ -43,7 +52,7 @@ def plot_SMF(entry):
         moustakas = np.genfromtxt('obs/Moustakas_2013_SMF_z0.csv', unpack=True, skip_header=1, delimiter=',')
         ax.plot(baldry[0], baldry[1]*10e-4, marker = '.', label='Baldry 2012', linestyle='None')
         ax.plot(moustakas[0], 10**moustakas[1], marker = '+', label='Moustakas 2013', linestyle='None')
-        ax.errorbar(baldry[0], baldry[1], fmt='none')
+        ax.errorbar(baldry[0], baldry[1], fmt='none')        
         
     #Axes settings
     ax.set_yscale('log')
@@ -51,7 +60,7 @@ def plot_SMF(entry):
     ax.tick_params(direction='in', which='both')
     plt.tight_layout()
     
-    plt.savefig("/scratch/hc2347/reports/60/SMF/z{}.png".format(z))
+    plt.savefig("/scratch/hc2347/reports/60/SMF/z{}_bins{}.png".format(z, bins))
 
     
 def plot_coldgas(mgas,mstar):
@@ -143,3 +152,78 @@ def plot_oxh(oxh,mstar):
     #ax.set_xlim(8.5,12)
     
     plt.savefig("/scratch/hc2347/reports/60/CenterCold_MZR_Oxh.png")
+    
+def plot_Moster(entry):
+    from pynbody.plot.stars import moster
+
+    z = entry["zred"]
+    xlabel = '$\\rm{log_{10}}(M_{200}/\\rm M_{\odot})$'
+    ylabel = '$\\rm{log_{10}}(M_{\star}/\\rm M_{\odot})$'
+    label = '$z \;=\; {}$'.format(z)
+    
+    fig, ax = plt.subplots(figsize=(7,5), dpi = 300)
+    
+    x = np.log10(np.array(entry["mvir"]))
+    y = np.log10(np.array(entry["mstar"]))
+    x, y = plot_tools.do_filter(x,y)
+    ax.hexbin(x, y, gridsize=50,cmap='Blues', bins='log')
+    
+    # Moster
+    xmasses = np.logspace(np.log10(min(entry['mvir'])),1+np.log10(max(entry['mvir'])),20)
+    ystarmasses, errors = moster(xmasses,float(entry["zred"]))
+    ax.plot(np.log10(xmasses),np.log10(np.array(ystarmasses)),color="Black",label="Moster 2013")
+    ax.plot(np.log10(xmasses),np.log10(np.array(ystarmasses)/np.array(errors)), linestyle='dashed', color = 'grey')
+    ax.plot(np.log10(xmasses),np.log10(np.array(ystarmasses)*np.array(errors)), linestyle='dashed', color = 'grey')
+    ax.fill_between(np.log10(xmasses),np.log10(np.array(ystarmasses)/np.array(errors)),y2=np.log10(np.array(ystarmasses)*np.array(errors)),
+                    color='grey', alpha=0.2)
+    
+    # NIHAO
+    nihao_mstar = plot_tools.nihao('Mstar',0)
+    nihao_mhalo = plot_tools.nihao('Mvir',0)
+    ax.scatter(np.log10(nihao_mhalo), np.log10(nihao_mstar), color='maroon', label = 'Nihao Classic')
+
+    
+    # Axes params
+    ax.axis([x.min(), x.max(), y.min(), y.max()])
+    ax.legend(frameon=False)
+    ax.tick_params(direction='in', which='both')
+    ax.set(xlabel=xlabel, ylabel=ylabel)
+    plt.tight_layout()
+    
+    plt.savefig("/scratch/hc2347/reports/60/MstarMhalo/Moster_z0.png")
+
+def plot_SFR(entry):
+    def salim(log_mstar):
+        sfr = 5.96e-11*10**((-1.35 + 1)*(log_mstar - 11.03))*np.exp(-10**(log_mstar - 11.03))
+        return sfr
+    whitacker = np.genfromtxt('/scratch/hc2347/main/visualization/obs/Whitacker_2014_SMF_z0.5.csv',unpack=True,skip_header=1,delimiter=',')
+
+    xlabel = '$\\rm{log_{10}}(M_{\star}/\\rm M_{\odot})$'
+    ylabel = '$\\rm{log_{10}(SFR/M_{\odot}\,yr^{-1})}$'
+    
+    fig, ax = plt.subplots(figsize=(7,5), dpi = 300)
+    
+    xmin = 7
+    xmax = 12
+    ymin = -1
+    ymax = 3
+    
+    x = np.log10(np.array(entry["mstar"]))
+    y = np.log10(np.array(entry["sfr_100"]))
+    #x, y = plot_tools.do_filter(x,y)
+    ax.set(xlim=(xmin, xmax), ylim=(ymin, ymax))
+
+    ax.hexbin(x, y,  gridsize = 100, bins = 'log', cmap = 'Blues')
+
+    nihao_x = plot_tools.nihao('Mstar',0)
+    nihao_y = plot_tools.nihao('SFR_100',0)
+    ax.scatter(np.log10(nihao_x), np.log10(nihao_y), color='maroon', label="NIHAO Classic")
+    #ax.plot(x,np.log10(salim(x)))
+    ax.plot(whitacker[0], whitacker[1], marker='s', linestyle='none', color = 'orange', label='Whitacker 2014')
+    ax.errorbar(whitacker[0], whitacker[1], yerr = whitacker[2], fmt='none', color = 'orange')
+    ax.set(xlabel=xlabel, ylabel=ylabel)
+    ax.tick_params(direction='in', which='both')
+    ax.legend(frameon=False)
+    plt.tight_layout()
+    
+    plt.savefig('/scratch/hc2347/reports/60/sfr_100_v1.png')
